cd7a355fde0d4baa82fad5f8541df5f64759b5aa
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-07-24 10:16:07 +0530

    ContainsDuplicate2 ques added

--- Array/ContainsDuplicate/ContainsDuplicate.java
+++ Array/ContainsDuplicate/ContainsDuplicate.java
@@ -1,9 +1,9 @@
-package Array;
+package ContainsDuplicate;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class isDuplicate {
+public class ContainsDuplicate {
     public static void main(String[] args) {
         int[] arr = {1,2,3,3,4,5,5};
         System.out.println(isDupli(arr));
