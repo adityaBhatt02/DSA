4bca83ad51415954fb24cadc17ac541d988a25c5
adityaBhatt02 <106445882+adityaBhatt02@users.noreply.github.com>
2026-08-03 17:17:13 +0530

    Refactor FindInMountainArray implementation

--- BinarySearchQuestions/MountainArray/FindInMountainArray.java
+++ BinarySearchQuestions/MountainArray/FindInMountainArray.java
@@ -1,63 +1,68 @@
-//package BinarySearchQuestions.MountainArray;//package BinarySearchQuestions;
-/////**
-//// * // This is MountainArray's API interface.
-//// * // You should not implement it, or speculate about its implementation
-//// * interface MountainArray {
-//// *     public int get(int index) {}
-//// *     public int length() {}
-//// * }
-//// */
-//
-//
-//public class FindInMountainArray {
-//
-//    class Solution {
-//        public int findInMountainArray(int target, MountainArray mountainArr) {
-//            int peak = peakOfMountain(mountainArr);
-//
-//            int small = binarySearch(target, mountainArr, 0, peak, true);
-//            if (small != -1) return small;
-//
-//            return binarySearch(target, mountainArr, peak + 1, mountainArr.length() - 1, false);
-//        }
-//
-//        public int peakOfMountain(MountainArray mountainArr) {
-//            int start = 0, end = mountainArr.length() - 1;
-//
-//            while (start < end) {
-//                int mid = start + (end - start) / 2;
-//                if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
-//                    start = mid + 1;
-//                } else {
-//                    end = mid;
-//                }
-//            }
-//            return start;
-//        }
-//
-//        public int binarySearch(int target, MountainArray mountainArr, int start, int end, boolean ascending) {
-//            while (start <= end) {
-//                int mid = start + (end - start) / 2;
-//                int midVal = mountainArr.get(mid);
-//
-//                if (midVal == target) return mid;
-//
-//                if (ascending) {
-//                    if (midVal < target) {
-//                        start = mid + 1;
-//                    } else {
-//                        end = mid - 1;
-//                    }
-//                } else {
-//                    if (midVal < target) {
-//                        end = mid - 1;
-//                    } else {
-//                        start = mid + 1;
-//                    }
-//                }
-//            }
-//            return -1;
-//        }
-//    }
-//
-//}
+package BinarySearchQuestions.MountainArray;
+///**
+// * // This is MountainArray's API interface.
+// * // You should not implement it, or speculate about its implementation
+// * interface MountainArray {
+// *     public int get(int index) {}
+// *     public int length() {}
+// * }
+// */
+
+
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peek = peek(mountainArr, 0, mountainArr.length() - 1);
+        if(mountainArr.get(peek) == target) return peek;
+
+        int ans1 = orderAgnoSearch(mountainArr, 0, peek - 1, target);
+        int ans2 = orderAgnoSearch(mountainArr, peek + 1, mountainArr.length() - 1, target);
+
+        if(ans1 != -1) return ans1;
+        if(ans2 != -1) return ans2;
+            
+        return -1;
+    }
+
+    public int orderAgnoSearch(MountainArray mountainArr, int start , int end, int target) {
+
+        if(mountainArr.get(start) < mountainArr.get(end)) {
+            while(start <= end) {
+                int mid = start + (end - start)/2;
+
+                if(mountainArr.get(mid) > target) {
+                    end = mid - 1;
+                }else if(mountainArr.get(mid) < target) {
+                    start = mid + 1;
+                }else {
+                    return mid;
+                }
+            }}
+        else {
+            while(start <= end) {
+                int mid = start + (end - start)/2;
+
+                if(mountainArr.get(mid) > target) {
+                     start = mid + 1;
+                }else if(mountainArr.get(mid) < target) {
+                    end = mid - 1;
+                }else {
+                    return mid;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int peek(MountainArray mountainArr, int start, int end) {
+        while(start < end) {
+            int mid = start + (end - start)/2;
+
+            if(mountainArr.get(mid) > mountainArr.get(mid + 1)) {
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
