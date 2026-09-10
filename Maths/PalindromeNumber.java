a0709e73e9f17d74effbfd85635db6c0dfa942f6
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-09-10 21:34:51 +0530

    implemented few more questions

--- Maths/PalindromeNumber.java
+++ Maths/PalindromeNumber.java
@@ -1,17 +1,32 @@
 package Maths;
 
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+
 public class PalindromeNumber {
-        public boolean isPalindrome(int x) {
-            if(x < 0) return false;
+    public boolean isPalindrome(int x) {
+        int original = x, reversed = 0;
 
-            int rev = reverse(x , 0);
-            return x == rev;
+        while(original > 0) {
+            reversed = reversed * 10 + original % 10;
+            original /= 10;
         }
-
-        public int reverse(int num , int rev) {
-            if(num == 0) return rev;
-
-            int rem = num % 10;
-            return reverse(num/10 , rev * 10 + rem);
-        }
+        return x == reversed;
     }
+}
--- String/PalindromeString.java
+++ String/PalindromeString.java
@@ -1,5 +1,4 @@
 package String;
-
 /*
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
@@ -12,18 +11,41 @@ Since an empty string reads the same forward and backward, it is a palindrome.
  */
 
 public class PalindromeString {
-        public boolean isPalindrome(String s) {
-            s = s.replaceAll("[^A-Za-z0-9]" , "");           // string s is alphanumeric
-            s = s.toLowerCase();
 
-            char[] c = s.toCharArray();
-            int start = 0 , end = c.length - 1;
+    // O(1) space complexity, just skipping those characters which are not alphanumeric.
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
 
-            while(start < end) {
-                if(c[start] != c[end]) return false;
-                start++;
-                end--;
+        while(left < right) {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
-            return true;
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+
+            left++;
+            right--;
         }
+        return true;
+    }
+
+
+// This approach is simple but taking O(n) space complexity by creating the character array of size n(size of the string)
+//        public boolean isPalindrome(String s) {
+//            s = s.replaceAll("[^A-Za-z0-9]" , "");           // string s is alphanumeric
+//            s = s.toLowerCase();
+//
+//            char[] c = s.toCharArray();
+//            int start = 0 , end = c.length - 1;
+//
+//            while(start < end) {
+//                if(c[start] != c[end]) return false;
+//                start++;
+//                end--;
+//            }
+//            return true;
+//        }
     }
