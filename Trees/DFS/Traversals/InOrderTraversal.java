ceba19dd649850259eb31d009fb04e17910d09f7
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-09-11 09:41:41 +0530

    added inorder and postorder traversals

---
+++ Trees/DFS/Traversals/InOrderTraversal.java
@@ -0,0 +1,16 @@
+public class InOrderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        inOrder(root, result);
+        return result;
+    }
+
+    private void inOrder(TreeNode root, List<Integer> result) {
+        if(root == null) return;
+
+        inOrder(root.left, result);
+        result.add(root.val);
+        inOrder(root.right, result);
+    }
+}
