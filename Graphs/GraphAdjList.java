4a3cdbe2518416ea5e3751a568825bc299cc1029
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-09-13 10:30:30 +0530

    added dfs traversal in graphs

--- Graphs/GraphAdjList.java
+++ Graphs/GraphAdjList.java
@@ -1,6 +1,10 @@
 public class GraphAdjList {
     private Map<Integer, List<Integer>> adjList;
 
+    // for dfs iterative traversal
+    private Set<Integer> visited = new HashSet<>();
+    private Stack<Integer> stack = new Stack<>();
+
     GraphAdjList() {
         this.adjList = new HashMap<>();
     }
@@ -26,6 +30,39 @@ public class GraphAdjList {
         // Remove edges pointing to the removed vertex
         for(List<Integer> neighbors : adjList.values()) {
             neighbors.remove((Integer) vertex);
+        }
+    }
+
+    /* Time Complexity -> O(V + E)
+       Space Complexity -> O(V)
+     */
+    public void dfsIterative(int vertex) {
+        stack.push(vertex);
+
+        while(!stack.isEmpty()) {
+            int currentVertex = stack.pop();
+            visited.add(currentVertex);
+
+            for(int neighbor : adjList.getOrDefault(currentVertex, Collections.emptyList())) {
+                if(!visited.contains(neighbor)) stack.push(neighbor);
+            }
+        }
+    }
+
+
+    /* Time Complexity -> O(V + E)
+       Space Complexity -> O(V)
+    */
+    public void dfs(int vertex) {
+        Set<Integer> visited = new HashSet<>();
+        dfsRecursive(vertex, visited);
+    }
+    public void dfsRecursive(int vertex, Set<Integer> visisted) {
+        visited.add(vertex);
+        System.out.print(vertex + " ");
+
+        for(int neighbor : adjList.getOrDefault(vertex, Collections.emptyList())) {
+            if(!visisted.contains(neighbor)) dfsRecursive(neighbor, visisted);
         }
     }
 }
