287d1e78ed8b2f969b40c02b550eb7e3f540ac79
adityaBhatt02 <aditya.adi02bhatt@gmail.com>
2026-09-14 18:48:18 +0530

    maxAreaOfIsland implemented

---
+++ Graphs/MaxAreaOfIsland.java
@@ -0,0 +1,56 @@
+/*
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Example 1:
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+ */
+
+public class MaxAreaOfIsland {
+    private int rows, cols;
+    private int currentArea;
+    private int maxArea;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        rows = grid.length;
+        cols = grid[0].length;
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(grid[i][j] == 1) dfs(i, j, grid);
+                currentArea = 0;
+            }
+        }
+        return maxArea;
+    }
+
+    private void dfs(int row, int col, int[][] grid) {
+        if(row < 0 || row >= rows || col < 0 || col >= cols) return;
+        if(grid[row][col] != 1) return;
+
+        grid[row][col] = 0;
+        currentArea++;
+
+        if(currentArea > maxArea) maxArea = currentArea;
+
+        dfs(row - 1, col, grid);
+        dfs(row + 1, col, grid);
+        dfs(row, col - 1, grid);
+        dfs(row, col + 1, grid);
+    }
+}
