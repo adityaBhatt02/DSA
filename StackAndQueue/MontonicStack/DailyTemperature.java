83587b3a140eb8d51f870b229b70ba40462a71f6
Aditya Bhatt <106445882+adityaBhatt02@users.noreply.github.com>
2026-09-19 17:24:27 +0530

    Refactor dailyTemperatures method implementation
    Refactor dailyTemperatures method to use a monotonic decreasing stack and improve logic for calculating days until warmer temperatures.

--- StackAndQueue/MontonicStack/DailyTemperature.java
+++ StackAndQueue/MontonicStack/DailyTemperature.java
@@ -10,18 +10,19 @@ there are two types of monotonic stacks - 1) monotonic increasing stack and 2) m
 so in this question we used monotonic decreasing stack as jaise he greatest mil rha hae saare chote hta de rhe hae.
  */
 public class DailyTemperature {
-        public int[] dailyTemperatures(int[] temperatures) {
-            int n = temperatures.length;
-            int[] arr = new int[n];
-            Stack<Integer> stack = new Stack<>();
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
 
-            for(int i = 0 ; i < n ; i++) {
-                while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
-                    int removed = stack.pop();
-                    arr[removed] = i - removed;
-                }
-                stack.push(i);
-            }
-            return arr;
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) stack.pop();
+            
+            if(!stack.isEmpty()) result[i] = stack.peek() - i;
+            stack.push(i);
         }
+        return result;
+    }
 }
